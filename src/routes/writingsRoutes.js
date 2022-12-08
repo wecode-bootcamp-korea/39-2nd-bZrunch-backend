b7e37f7afd268b1950b6915c2bf78d086b2ed14f
MatheGoD <mathe1303@gmail.com';:Wq:wq>
2022-12-08 16:12:09 +0900

    [FEATURE] 글 추가

--- src/routes/writingsRoutes.js
+++ src/routes/writingsRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const { writingsController } = require('../controllers');
-
+const { loginRequired } = require('../utils/auth');
 const writingsRoutes = express.Router();
 
 writingsRoutes.get('/search', writingsController.searchTitle);
 writingsRoutes.get('', writingsController.getWritings);
 writingsRoutes.get('/:writings_id', writingsController.getwritingInfo);
+writingsRoutes.post('', loginRequired, writingsController.createWriting);
 
 module.exports = { writingsRoutes };
