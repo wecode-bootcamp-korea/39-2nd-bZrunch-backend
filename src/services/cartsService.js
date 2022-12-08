a5d9a2ec1183ae038fa650316e57e9a393d86380
MatheGoD <mathe1303@gmail.com';:Wq:wq>
2022-12-08 14:50:53 +0900

    [FEATURE] 장바구니 추가

--- src/services/cartsService.js
+++ src/services/cartsService.js
@@ -4,4 +4,13 @@ const showCart = async (user_id) => {
     return await cartsDao.showCart(user_id);
 };
 
-module.exports = { showCart };
+const addCart = async (user_id, writing_id) => {
+    const check = await cartsDao.checkCart(user_id, writing_id);
+    if (check != '0') {
+        return '이미 추가되었습니다.';
+    }
+    await cartsDao.addCart(user_id, writing_id);
+    return '장바구니에 추가되었습니다.';
+};
+
+module.exports = { showCart, addCart };
